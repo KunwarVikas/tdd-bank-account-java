5d40151f352b9becce177bef475d966f440232c1
Kunwar <kunwar.nitw@gmail.com>
2020-07-16 19:22:56 +0530

    First test cases

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance;
+    public int getBalance() {
+        return 0;
+    }
+
+    public int depositAnAmountToIncreaseTheBalance(int amount) {
+        balance+=amount;
+        return balance;
+    }
 }
